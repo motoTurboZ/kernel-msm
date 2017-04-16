f505ec70d86a420d3a3a0c9c046d70ee5988c447
Erfan Abdi <erfangplus@gmail.com>
2017-04-16 17:42:46 +0430

    Setup Building script and defconfig for CM

--- build_kernel_GRIFFIN.sh
+++ build_kernel_GRIFFIN.sh
@@ -3,11 +3,6 @@ export KERNELDIR=`pwd`
 export CROSS_COMPILE="aarch64-linux-androidkernel-"
 export ARCH=arm64
 
-#Set Correct Path Here
-export WIFIDRIVER=$KERNELDIR/../vendor-qcom-opensource-wlan-qcacld-2.0
-export GREYBUSDRIVER=$KERNELDIR/../motorola-external-greybus
-export V4L2DRIVER=$KERNELDIR/../motorola-external-v4l2_hal
-
 if [ ! -f $KERNELDIR/.config ];
 then
 	make defconfig TurboZ_defconfig
@@ -18,13 +13,8 @@ fi
 cd $KERNELDIR/
 make -j10 || exit 1
 
-mkdir -p $KERNELDIR/BUILT_GRIFFIN/modules
-mkdir -p $KERNELDIR/BUILT_GRIFFIN/modules/qca_cld
-
 rm $KERNELDIR/BUILT_GRIFFIN/modules/*
 rm $KERNELDIR/BUILT_GRIFFIN/zImage
-
-find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT_GRIFFIN/modules/ \;
 
 if [ ! -f $KERNELDIR/arch/arm64/boot/zImage ];
 then
@@ -32,19 +22,3 @@ then
 else
 	cp $KERNELDIR/arch/arm64/boot/zImage $KERNELDIR/BUILT_GRIFFIN/zImage
 fi
-
-make -j10 M=$WIFIDRIVER WLAN_ROOT=$WIFIDRIVER MODNAME=wlan BOARD_PLATFORM=msm8996 CONFIG_QCA_CLD_WLAN=m WLAN_OPEN_SOURCE=1 modules
-cp $WIFIDRIVER/wlan.ko $KERNELDIR/BUILT_GRIFFIN/modules/qca_cld/qca_cld_wlan.ko
-rm $KERNELDIR/BUILT_GRIFFIN/modules/qca_cld_wlan.ko
-
-cd $GREYBUSDRIVER 
-make -j10 -C $KERNELDIR M=$GREYBUSDRIVER
-find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT_GRIFFIN/modules/ \;
-
-cd $V4L2DRIVER
-make -j10 -C $KERNELDIR M=$V4L2DRIVER
-find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT_GRIFFIN/modules/ \;
-
-cd $KERNELDIR
-${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT_GRIFFIN/modules/*
-${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT_GRIFFIN/modules/qca_cld/*
